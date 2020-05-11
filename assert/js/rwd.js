25653a4ab8d059c131b21fca8424585da59c36ac
Sheng-Jie106 <55180712+Sheng-Jie106@users.noreply.github.com>
2020-05-11 14:16:50 +0800

    視窗可大小可移動不跑版

--- assert/js/rwd.js
+++ assert/js/rwd.js
@@ -1,13 +1,10 @@
-//調整main-padding
-// var main_padding=document.getElementsByTagName("main").item(0);
-// var width=window.screen.width;
-// if(width<992){
-//     main_padding.style.padding="25px";
-// }
 function widt(){
     document.getElementById("main").style.display="none";
 }
 //調整article,section長度
+window.onresize=function(){
+    wid();
+}
 function wid(){
     var wid=window.screen.width;
     if(wid>992){
